4f90777748ac9b4a8e16a28a606aeb10422b3197
laksh <laksh@DESKTOP-VA1N5IB>
2018-06-26 11:28:34 +0530

    first commit

---
+++ selenium_practice/src/main/java/com/practice_selenium/MyClass.java
@@ -0,0 +1,19 @@
+package com.practice_selenium;
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.chrome.ChromeDriver;		
+
+public class MyClass {				
+    		
+    public static void main(String[] args) {									
+        String baseUrl = "http://demo.guru99.com/test/link.html";					
+        System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");					
+        WebDriver driver = new ChromeDriver();					
+        		
+        driver.get(baseUrl);					
+        driver.findElement(By.linkText("click here")).click();					
+        System.out.println("title of page is: " + driver.getTitle());							
+        driver.quit();			
+    }		
+
+}
